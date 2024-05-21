6edb964d166b215f9d0940aa1c0aa5f1ad56b540
Charley Rutledge <99267310+CharleyRutledge@users.noreply.github.com>
2024-05-21 02:56:52 +0100

    Added Driver Constructor (#1)

--- SwagLabsAutomation/UI_Tests/Login Tests/Login_Tests.cs
+++ SwagLabsAutomation/UI_Tests/Login Tests/Login_Tests.cs
@@ -7,13 +7,14 @@ namespace SwagLabsAutomation.UI_Tests
 {
     public class Login_Tests : IClassFixture<WebDriverFixture>
     {
+        #region Driver Constructor
         private readonly IWebDriver _driver;
 
         public Login_Tests(WebDriverFixture fixture)
         {
             _driver = fixture.Driver;
         }
-
+        #endregion Driver Constructor
 
         [Fact]
         public void LoginAsStandardUser()
